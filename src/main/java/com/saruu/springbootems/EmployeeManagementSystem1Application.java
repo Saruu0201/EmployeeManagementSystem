8f50458ce41b371288d0ea5d6f899118a506c6a2
Saruu <saraswathina333@gmail.com>
2023-09-15 16:09:50 +0530

    Final Commit

--- src/main/java/com/saruu/springbootems/EmployeeManagementSystem1Application.java
+++ src/main/java/com/saruu/springbootems/EmployeeManagementSystem1Application.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = {"com.saruu.springbootems.entities", "com.saruu.springbootems.controller","com.saruu.springbootems.services", "com.saruu.springbootems.repository"})
 @EnableJpaRepositories(basePackages = "com.saruu.springbootems.repository")
 public class EmployeeManagementSystem1Application {
-
+// This is my first Spring Boot Application
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystem1Application.class, args);
 	}
